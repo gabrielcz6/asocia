6db48d8b237845f04d817a8afb50862a08e296c5
Jerry Yenque <jerry.yenque@hotmail.com>
2024-12-07 23:45:31 -0500

    user collection

---
+++ db/inject/users.js
@@ -0,0 +1,29 @@
+db.users.insertOne({
+    "usuario": "decano1",
+    "password": "decano123",
+    "rol": "decano"
+});
+
+db.users.insertOne({
+    "usuario": "secretaria1",
+    "password": "secre123",
+    "rol": "secretaria"
+});
+
+db.users.insertOne({
+    "usuario": "profesor1",
+    "password": "profesor123",
+    "rol": "profesor"
+});
+
+db.users.insertOne({
+    "usuario": "profesor2",
+    "password": "profesor456",
+    "rol": "profesor"
+});
+
+db.users.insertOne({
+    "usuario": "secretaria2",
+    "password": "secre456",
+    "rol": "secretaria"
+});
